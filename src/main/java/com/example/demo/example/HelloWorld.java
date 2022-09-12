a03e0c81aa78d9829b90f5652ee079e0722ce83d
jothishiva123 <113039401+jothishiva123@users.noreply.github.com>
2022-09-12 14:22:59 +0530

    Updated

--- src/main/java/com/example/demo/example/HelloWorld.java
+++ src/main/java/com/example/demo/example/HelloWorld.java
@@ -3,6 +3,5 @@ package com.example.demo.example;
 public class HelloWorld {
 	public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-		logger.log("My Message");
     }
 }
