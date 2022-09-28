d3d7f1a0d9321390a12dae2b5b373674e1943cfa
jothishiva123 <113039401+jothishiva123@users.noreply.github.com>
2022-09-28 16:03:43 +0530

    Create javamaven.groovy

---
+++ vars/javamaven.groovy
@@ -0,0 +1,58 @@
+def call(body) {
+
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    def agentLabel = null
+
+    node {
+        stage('Checkout and set agent'){
+            checkout scm
+            agentLabel = getProperty(key:"agentLabel",file:"javaMaven.properties")
+            echo "found the agentLabel value of: ${agentLabel}"
+        }
+    }
+
+    pipeline {
+        agent { label "$agentLabel"}
+        stages {
+            stage("echo parameters") {
+                steps {
+                    sh "env | sort"
+                }
+            }
+            stage("Prepare Build Environment") {
+                steps {
+                    prepareBuildEnvironment()
+                    helloWorld(name: "prepareBuildEnvironment")
+                    helloWorldExternal()
+                }
+            }
+            stage("SonarQube Scan") {
+                when {
+                    branch 'master'
+                }
+                steps {
+                    echo 'scan'
+                }
+            }
+            stage("Build Application") {
+                steps {
+                    sh "./mvnw clean install"
+                }
+            }
+            stage("Publish Artifacts") {
+                steps {
+                    publishArtifacts(name: "publishArtifacts")
+                }
+            }
+            stage("Deploy Application") {
+                steps {
+                    deployApplication(name: "deployApplication")
+                }
+            }
+        }
+    }
+}
